f31d9ce1807b377dc4bf4085160171d4f12b802e
Destin <berniedestin7@gmail.com>
2025-06-19 20:44:14 -0400

    Add base Spot component

---
+++ Setback/components/game/SetbackLayout.tsx
@@ -0,0 +1,25 @@
+import { View, StyleSheet } from 'react-native';
+import { Circle, Svg } from 'react-native-svg'
+//       HHHH      
+//        OO       
+//       OV O      
+//      O V  O     
+//     D  V   D    
+//    O   V    O   
+// H O          O H
+// HO     TT VVVVOH
+// HOVVVV TT     OH
+// H O          O H
+//    O    V   O   
+//     D   V  D    
+//      O  V O     
+//       O VO      
+//        OO       
+//       HHHH      
+
+
+export default function SetbackLayout() {
+  return (
+    <View></View>
+  )
+}
---
+++ Setback/components/game/Spot.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Circle } from 'react-native-svg';
+
+
+export default function Spot() {
+  return (
+    <Circle />
+  )
+}
